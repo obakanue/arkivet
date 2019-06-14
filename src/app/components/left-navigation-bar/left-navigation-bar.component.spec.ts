bef8059913b444c9347a0d79206ed816627cc2dd
Sofi <sofi.flinck@gmail.com>
2019-06-14 18:44:39 +0200

    first commit

---
+++ src/app/components/left-navigation-bar/left-navigation-bar.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LeftNavigationBarComponent } from './left-navigation-bar.component';
+
+describe('LeftNavigationBarComponent', () => {
+  let component: LeftNavigationBarComponent;
+  let fixture: ComponentFixture<LeftNavigationBarComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LeftNavigationBarComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LeftNavigationBarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/left-navigation-bar/left-navigation-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-left-navigation-bar',
+  templateUrl: './left-navigation-bar.component.html',
+  styleUrls: ['./left-navigation-bar.component.css']
+})
+export class LeftNavigationBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
