7223094182e83aa25fd8d9ec29a5548e8aecaf93
Sofi <sofi.flinck@gmail.com>
2019-06-14 20:34:53 +0200

    adds front page component and edited footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LeftNavigationBarComponent } from './components/left-navigation-bar/left-navigation-bar.component';
+import { FrontPageComponent } from './components/front-page/front-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LeftNavigationBarComponent
+    LeftNavigationBarComponent,
+    FrontPageComponent
   ],
   imports: [
     BrowserModule,
